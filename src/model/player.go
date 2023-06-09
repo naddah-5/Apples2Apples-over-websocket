6afa9df6e53343242dec31b11275b8023cb51fef
naddah-5 <andersoskardahl@hotmail.com>
2023-06-09 18:08:33 +0200

    mw

--- src/model/player.go
+++ src/model/player.go
@@ -1,1 +1,26 @@
 package model
+
+type Player struct {
+	name string
+	bot bool
+	hand []Card
+	points []Card
+}
+
+func NewPlayer(playerName string, bot bool) Player {
+	player := Player{
+		name: playerName,
+		bot: bot,
+		hand: *new([]Card),
+		points: *new([]Card),
+	}
+	return player
+}
+
+func (p *Player) ID() string {
+	return p.name
+}
+
+func (p *Player) Bot() bool {
+	return p.bot
+}
